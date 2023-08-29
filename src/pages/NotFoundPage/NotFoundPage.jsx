4540f9c1938aa752d035e3ea6b4345b6ff36c050
Maeve <mariya.kiryakulova@gmail.com>
2023-08-29 22:44:09 +0100

    add-404-Not-Found-page
    Created the NotFoundPage component, displaying an image and a link back to the home page using react-router-dom's Link component. This component serves as the custom 404 page for when a user accesses a non-existent route, improving the application's user experience.

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <img/>
+            <Link to='/'>To home</Link>
+        </div>
+    )
+}
+
+export default NotFoundPage

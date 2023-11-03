2a84e8874612021b0910e472924b30b609407c03
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2023-11-03 00:58:27 +0000

    update-add-contact-form
    Revised the 'AddContactForm' component, ensuring that it now properly initializes the 'name' and 'number' state variables using the useState hook

--- src/pages/Phonebook/components/AddContactForm.jsx
+++ src/pages/Phonebook/components/AddContactForm.jsx
@@ -1,17 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { usePostContactMutation, useGetContactsQuery } from '../../../redux/contacts/contactsOperations';
 
 
 const AddContactForm = () => {
-    const [name, setName] = useState();
-    const [number, setNumber] = useState();
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-    const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        name === 'name'?setName(value) : setNumber(value)
+  const {data} = useGetContactsQuery();
+
+  const [postContacts, {isLoading}] = usePostContactMutation();
+
+  const handleInputChange = (event) => {
+      const {name, value} = event.target;
+      name === 'name'?setName(value) : setNumber(value)
+  };
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    if (data) {
+      const isInContacts = data?.find(contact => contact.name === name)
+      if (isInContacts) {
+        return alert(`${name} is already in contacts`)
+      }
+    }
+
+    const contactToAdd = {
+      name, 
+      number
     };
 
+    try {
+      await postContacts(contactToAdd)
+    } catch (error) {
+      console.log(error);
+    };
+
+    resetForm()
+  }
+
+  const resetForm = () => {
+    setName('');
+    setNumber('')
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <label>Name<input onChange={handleInputChange} value={name} name="name"/></label>
         <label>Number<input onChange={handleInputChange} value={number} name="number"/></label>
         <button type="submit">Save</button>

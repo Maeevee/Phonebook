a2d403a48b3bc26d85d4b39d2adbc9a84595c534
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2023-11-05 00:10:49 +0000

    fix-deploy

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NotFoundPage = () => {
     return (
         <div>
-            <img/>
+            <img alt='not found'/>
             <Link to='/'>To home</Link>
         </div>
     )

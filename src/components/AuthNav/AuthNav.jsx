000b62b6fc15ed0c75f737a6d0a08f05741d5111
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2023-11-11 13:37:00 +0000

    fix-err

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,11 +1,33 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import { nav } from "../../helpers/nav"
+import styles from './AuthNav.module.css'
 
 const AuthNav = () => {
+
+  const location = useLocation(); 
+  // const isCurrentPage = location.pathname === href;
+  // const buttonClass = isCurrentPage
+  //         ? `${styles.button} ${styles.activeButton}`
+  //         : `${styles.button} ${styles.inactiveButton}`;
+
   return (
-    <ul>
-        {nav.map( ({id, name, href}) => <li key={id}><NavLink to={href}>{name}</NavLink></li>)}
-    </ul>
+    <div className={styles.container}>
+        {nav.map( ({id, name, href}) => {
+        // Check if the current route matches the 'href' value
+        const isCurrentPage = location.pathname === href;
+
+        // Define separate CSS classes for active and inactive buttons
+        const buttonClass = isCurrentPage
+          ? `${styles.button} ${styles.activeButton}`
+          : `${styles.button} ${styles.inactiveButton}`;
+
+        return (
+          <NavLink className={styles.link} key={id} to={href}>
+            <button className={buttonClass}>{name}</button>
+          </NavLink>
+        );
+      })}
+    </div>
   )
 }
 
